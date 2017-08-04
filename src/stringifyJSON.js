d52bbb7e0920de628122971b34242ab4fda3ae99
Jonathan Lee <jonathanleeee@gmail.com>
2017-08-03 19:03:26 -0700

    "working on JSON"

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,44 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if ( typeof obj === 'number') {
+  	return obj.toString();
+  }
+  if ( obj === null) {
+  	return 'null';
+  }
+  if ( typeof obj ===  'boolean') {
+  	return obj.toString();
+  }
+  if ( typeof obj === 'string') {
+  	return '"' + obj + '"';
+  }
+  if ( Array.isArray(obj)) {
+  	var result = '';
+  	for ( var i = 0; i < obj.length; i++) {
+  		if ( i === 0) {
+  		result += stringifyJSON(obj[i]);
+  	  } else {
+  	  	result += "," + stringifyJSON(obj[i]);
+  	  } 
+  	}
+  	return '[' + result + ']';
+  } else {
+  	var result2 = '';
+  	var counter = 0;
+  	for ( var key in obj) {
+  		if (obj.hasOwnProperty(key)) {
+  		if ( counter === 0 ) {
+  		    result2 += stringifyJSON(key) + ':' + stringifyJSON(obj[key]);
+  		}else{
+  			result2 += ',' + stringifyJSON(key) + ':' + stringifyJSON(obj[key]);
+  		}
+  		counter ++;
+  		}	
+  	}
+  	return '{' + result2 + '}';
+  }
+
+
+  
 };
